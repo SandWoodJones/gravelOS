a4dee9677a4452cd010197601fdf96dd45c8a54d
SandWood Jones <sandwoodjones@outlook.com>
2024-09-22 15:03:00 -0300

    added wiki's zsh-autocomplete fix

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -9,6 +9,8 @@
       historySubstringSearch.searchDownKey = "$terminfo[kcud1]";
       historySubstringSearch.searchUpKey = "$terminfo[kcuu1]";
       history.path = "${config.xdg.stateHome}/zsh_history";
+
+      initExtra = "bindkey \"''$\{key[Up]\}\" up-line-or-search"; # https://wiki.nixos.org/wiki/Zsh#Zsh-autocomplete_not_working
     };
   };
 }

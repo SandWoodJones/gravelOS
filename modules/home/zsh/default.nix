6366475095c52ff966f5e5f6bad68b4cf8c6bcae
SandWood Jones <sandwoodjones@outlook.com>
2024-11-06 20:26:33 -0300

    added zsh completion rule for more convenient case sensitiveness

--- modules/home/zsh/default.nix
+++ modules/home/zsh/default.nix
@@ -10,6 +10,12 @@
       historySubstringSearch.searchUpKey = "$terminfo[kcuu1]";
       history.path = "${config.xdg.stateHome}/zsh_history";
 
+      # https://stackoverflow.com/a/69014927
+      completionInit = ''
+        zstyle ':completion:*' matcher-list ''' 'm:{a-zA-Z}={A-Za-z}' 'r:|=*' 'l:|=* r:|=*'
+        autoload -U compinit && compinit
+      '';
+
       initExtra = "bindkey \"''$\{key[Up]\}\" up-line-or-search"; # https://wiki.nixos.org/wiki/Zsh#Zsh-autocomplete_not_working
     };
   };

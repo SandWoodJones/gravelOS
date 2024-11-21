7528592a77527708b96fcd16c9fd08350c170c33
SandWood Jones <sandwoodjones@outlook.com>
2024-11-21 11:34:48 -0300

    disabled git only ssh origins when wifi is enabled to avoid port blocking, installed gh-cli

--- modules/nixos/git/default.nix
+++ modules/nixos/git/default.nix
@@ -1,30 +1,37 @@
-{ pkgs, ... }: {
-  programs.git = {
-    enable = true;
+{ lib, config, pkgs, ... }: {
+  config = {
+    programs.git = {
+      enable = true;
 
-    # https://jvns.ca/blog/2024/02/16/popular-git-config-options/
-    config = {
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      merge.conflictStyle = "zdiff3";
-      commit.verbose = true;
-      rerere.enabled = true;
-      diff.algorithm = "histogram";
-      url."git@github.com:".insteadOf = [ "https://github.com/" ];
-      branch.sort = "-committerdate";
+      # https://jvns.ca/blog/2024/02/16/popular-git-config-options/
+      config = {
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        merge.conflictStyle = "zdiff3";
+        commit.verbose = true;
+        rerere.enabled = true;
+        diff.algorithm = "histogram";
+        url."git@github.com:".insteadOf = lib.mkIf (!config.gravelOS.networking.wifi.enable) [ "https://github.com/" ];
+        branch.sort = "-committerdate";
 
-      core.pager = "${pkgs.delta}/bin/delta";
-      interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
-      delta.navigate = true;
+        core.pager = "${pkgs.delta}/bin/delta";
+        interactive.diffFilter = "${pkgs.delta}/bin/delta --color-only";
+        delta.navigate = true;
 
-      transfer.fsckObjects = true;
-      fetch.fsckObjects = true;
-      receive.fsckObjects = true;
+        transfer.fsckObjects = true;
+        fetch.fsckObjects = true;
+        receive.fsckObjects = true;
 
-      alias = {
-        df = "diff";
-        dfs = "diff --staged";
+        alias = {
+          df = "diff";
+          dfs = "diff --staged";
+        };
+
+        credential."https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+        credential."https://gist.github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
       };
     };
+
+    environment.systemPackages = [ pkgs.gh ];
   };
 }

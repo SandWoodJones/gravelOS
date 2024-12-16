cbac7f57f59170249a13082209df20fdaa51a3fa
SandWood Jones <sandwoodjones@outlook.com>
2024-12-16 12:50:52 -0300

    installed r2modman

--- modules/home/gaming/default.nix
+++ modules/home/gaming/default.nix
@@ -1,6 +1,7 @@
 { lib, config, pkgs, ... }: lib.mkIf config.gravelOS.desktop.gaming.enable {
   home.packages = with pkgs; [
     mangohud
+    r2modman
   ];
 
   home.sessionVariables.WINEPREFIX = "${config.xdg.dataHome}/wine";

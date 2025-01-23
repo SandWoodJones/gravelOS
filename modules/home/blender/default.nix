f9bf525cf095fcd27cc834be3b8ef3b6b747cc30
SandWood Jones <sandwoodjones@outlook.com>
2025-01-23 11:52:14 -0300

    deleted the blender module for now, need to remake it later

--- modules/home/blender/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-let
-  cfgDsk = config.gravelOS.desktop;
-in {
-  imports = [ ./openmw.nix ];
-  config = lib.mkIf (cfgDsk.enable && cfgDsk.blender.enable) {
-    home.packages = [ pkgs.blender ];
-  };
-}

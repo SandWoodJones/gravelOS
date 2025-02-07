4c6b564ab197bf14b927b8b5aad16b578f327467
SandWood Jones <sandwoodjones@outlook.com>
2025-02-06 21:22:34 -0300

    installed sops, made user passwords declarative

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -6,27 +6,43 @@ let
 
   cfg = config.gravelOS.user;
 in {
-  options.gravelOS.user.createSWJ = lib.mkOption {
-    default = false;
-    example = true;
-    description = "Whether gravelOS should create the SandWood Jones user.";
-    type = lib.types.bool;
+  options.gravelOS.user = {
+    createSWJ = lib.mkOption {
+      default = false;
+      example = true;
+      description = "Whether gravelOS should create the SandWood Jones user.";
+      type = lib.types.bool;
+    };
+
+    managePasswords = lib.mkOption {
+      default = false;
+      example = true;
+      description = "Whether passwords should be set through secrets. WARNING: make sure to have secrets working before enabling.";
+      type = lib.types.bool;
+    };
   };
 
   config = {
     users = {
-      users.swj = lib.mkIf cfg.createSWJ {
-        isNormalUser = true;
-        description = "SandWood Jones";
-        openssh.authorizedKeys.keyFiles = [ "${sshKeysPath}/id_swj.pub" ];
-        useDefaultShell = true;
-        extraGroups = [
-          "wheel"
-          "video"
-        ] ++ ifPresent [
-          "networkmanager"
-          "gamemode"
-        ];
+      mutableUsers = !cfg.managePasswords;
+    
+      users = {
+        root.hashedPasswordFile = lib.mkIf cfg.managePasswords config.sops.secrets.root-password.path;
+        
+        swj = lib.mkIf cfg.createSWJ {
+          hashedPasswordFile = lib.mkIf cfg.managePasswords config.sops.secrets.swj-password.path;
+          isNormalUser = true;
+          description = "SandWood Jones";
+          openssh.authorizedKeys.keyFiles = [ "${sshKeysPath}/id_swj.pub" ];
+          useDefaultShell = true;
+          extraGroups = [
+            "wheel"
+            "video"
+          ] ++ ifPresent [
+            "networkmanager"
+            "gamemode"
+          ];
+        };
       };
     };
   };

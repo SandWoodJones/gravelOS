b86eab6bf784d95dcc87f970db2e62c5e7c89d32
SandWood Jones <sandwoodjones@outlook.com>
2025-02-13 10:26:41 -0300

    window movement bindings for hyprland

--- modules/home/hyprland/binds.nix
+++ modules/home/hyprland/binds.nix
@@ -5,14 +5,26 @@ let
       "$mod+SHIFT, ${workspace}, movetoworkspace, ${workspace}"
     ]
   ) 9);
+
+  windowMovementBinds = let
+    directions = [
+      { key = "H"; dir = "l"; } { key = "L"; dir = "r"; } { key = "K"; dir = "u"; } { key = "J"; dir = "d"; }
+    ];
+  in builtins.concatMap ({ key, dir }: [
+    "$mod, ${key}, movefocus, ${dir}" 
+    "$mod+SHIFT, ${key}, movewindow, ${dir}" 
+    "$mod+CTRL, ${key}, swapwindow, ${dir}" 
+  ]) directions;
 in [  
   "CTRL+ALT, q, exec, uwsm stop"
   "$mod+SHIFT, r, execr, hyprctl reload"
   "ALT, F4, killactive,"
+
+  "$mod, space, togglefloating,"
 
   "$mod, Return, exec, uwsm app -- $terminal"
   "$mod, apostrophe, exec, uwsm app -- $launcher"
 
   "$mod, 0, workspace, 10"
   "$mod+SHIFT, 0, movetoworkspace, 10"
-] ++ workspaceBinds
+] ++ workspaceBinds ++ windowMovementBinds

b829473b16208dd58f43f9091e1014bfa8010333
SandWood Jones <sandwoodjones@outlook.com>
2025-04-23 00:05:09 -0300

    direnv: added settings

--- modules/nixos/cli/default.nix
+++ modules/nixos/cli/default.nix
@@ -22,7 +22,14 @@ in {
 
   config = lib.mkIf cfg.configEnable {
     programs = {
-      direnv = { enable = true; silent = true; };
+      direnv = {
+        enable = true;
+        silent = true;
+        settings.global = {
+          load_dotenv = true;
+          strict_env = true; # TODO: remove when it becomes the default
+        };
+      };
       command-not-found.enable = false;
 
       nix-index.enable = cfg.nix-index.enable;

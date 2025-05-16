4bdc26e2c7a4ba09de011d1edbc31eb641ff9879
SandWood Jones <sandwoodjones@outlook.com>
2025-05-16 15:52:40 -0300

    refactored hyprland

---
+++ modules/nixos/desktop/hyprland/default.nix
@@ -0,0 +1,36 @@
+# TODO: replace kwallet with gnome-keyring
+
+{
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.gravelOS.desktop.hyprland;
+in
+{
+  options.gravelOS.desktop.hyprland = {
+    enable = lib.mkEnableOption "Hyprland.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+    };
+
+    # Enable default PAM rules for hyprlock
+    security.pam.services.hyprlock = { };
+
+    hardware.brillo.enable = true;
+    services.playerctld.enable = true;
+
+    environment.sessionVariables = {
+      GDK_BACKEND = "wayland";
+      QT_QPA_PLATFORM = "wayland";
+      MOZ_ENABLE_WAYLAND = "1";
+      NIXOS_OZONE_WL = "1";
+      ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    };
+  };
+}

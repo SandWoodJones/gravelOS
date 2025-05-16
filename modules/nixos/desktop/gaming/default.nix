26b2f2464e7eef1cc39740a89207ca3bed2455d6
SandWood Jones <sandwoodjones@outlook.com>
2025-05-16 12:46:54 -0300

    refactored gaming

---
+++ modules/nixos/desktop/gaming/default.nix
@@ -0,0 +1,27 @@
+# TODO: look into steam millenium
+
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.gravelOS.desktop.gaming;
+in
+{
+  options.gravelOS.desktop.gaming = {
+    enable = lib.mkEnableOption "general gaming support.";
+    steam.enable = lib.mkEnableOption "Steam.";
+  };
+
+  config = lib.mkIf cfg.enable {
+    hardware.graphics.enable32Bit = true;
+
+    programs.steam = lib.mkIf cfg.steam.enable {
+      enable = true;
+      package = pkgs.gravelOS.steam-silent;
+      localNetworkGameTransfers.openFirewall = true;
+    };
+  };
+}

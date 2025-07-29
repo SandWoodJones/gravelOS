85365ef7aaf76db855383f0ba0da7996c77af58c
SandWood Jones <sandwoodjones@outlook.com>
2025-07-29 02:06:31 -0300

    nixos: added home-manager command

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,9 +1,11 @@
 {
+  pkgs,
   lib,
   inputs,
   ...
 }:
 {
   inherit (lib.gravelOS) nix sops;
+  environment.systemPackages = [ pkgs.home-manager ];
   documentation.nixos.extraModules = builtins.attrValues inputs.self.nixosModules;
 }

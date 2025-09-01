90202135cb0daa434dae5b0e336747a03c93a6ce
SandWood Jones <sandwoodjones@outlook.com>
2025-09-01 13:54:54 -0300

    flake: updated system

--- modules/home/system/networking/default.nix
+++ modules/home/system/networking/default.nix
@@ -28,7 +28,21 @@ in
 
     programs.ssh = {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks = {
+        "*" = {
+          forwardAgent = false;
+          serverAliveInterval = 0;
+          serverAliveCountMax = 3;
+          compression = false;
+          addKeysToAgent = "no";
+          hashKnownHosts = false;
+          userKnownHostsFile = "~/.ssh/known_hosts";
+          controlMaster = "no";
+          controlPath = "~/.ssh/master-%r@%n:%p";
+          controlPersist = "no";
+        };
+
         git = lib.mkIf cfg.ssh.git.enable {
           host = "github.com gitlab.com";
           identitiesOnly = true;
